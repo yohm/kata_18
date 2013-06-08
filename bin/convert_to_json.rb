b8c3d9eb337a748fc7f1cfed8690488aa94843ae
Yohsuke <flyingcamelspecial@gmail.com>
2013-06-08 15:29:59 +0900

    add a script to convert dependency text to json

---
+++ bin/convert_to_json.rb
@@ -0,0 +1,11 @@
+require File.join( File.dirname(__FILE__), '../lib/dependencies' )
+
+unless ARGV[0]
+  $stderr.puts "Specify dependency file"
+  raise "No input"
+end
+
+input = File.open(ARGV[0], 'r').read
+dep = Dependencies.new
+dep.parse(input)
+$stdout.puts dep.to_heb_json
